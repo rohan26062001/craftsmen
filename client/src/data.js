afdc6a76adacfcf6b26392205083500cc71acee9
Rahul_Roy_21 <royrahul0921@gmail.com>
2022-03-05 03:41:50 +0530

    Added Custom Product Data for React-Routing on Product-Page

--- client/src/data.js
+++ client/src/data.js
@@ -1,89 +1,257 @@
-export const sliderItems = [{
-        id: 1,
-        img: "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+export const products = [
+  {
+    id: 1,
+    img: "https://rukminim1.flixcart.com/image/416/416/kgiaykw0/perfume/4/v/d/100-perfume-eau-de-parfum-100-ml-for-men-eau-de-parfum-villain-original-imafwqcjvgsxzzjx.jpeg?q=70",
 
-        title: "Summer Sale",
-        desc: "Don't Compromise On Style! Get Flat 30% OFF for New Arrivals!",
-        isActive: true,
-    },
-    {
-        id: 2,
-        img: "https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    category: ["Beauty & Grooming", "VILLAIN Hydra Perfume"],
+    name: "VILLAIN Hydra Perfume Eau de Parfum - 20 ml (For Men)",
+    ratingCount: 4081,
+    reviews: [
+      {
+        rating: 5,
+        desc: "Such a nice perfume, the fragrance is great and long lasting. This product was suggested by a friend which turned up extremely good. Now I have suggested to my connections. Loved it !!",
+        userName: "Anupam Sharma",
+        dateofReview: "Aug, 2021",
+        userCity: "Gwalior",
+        isCertified: true,
+      },
+      {
+        rating: 4.7,
+        desc: "Such a nice perfume, the fragrance is great and long lasting. This product was suggested by a friend which turned up extremely good. Now I have suggested to my connections. Loved it !!",
+        userName: "Anupam Sharma",
+        dateofReview: "Aug, 2021",
+        userCity: "Gwalior",
+        isCertified: true,
+      },
+      {
+        rating: 4.7,
+        desc: "Such a nice perfume, the fragrance is great and long lasting. This product was suggested by a friend which turned up extremely good. Now I have suggested to my connections. Loved it !!",
+        userName: "Anupam Sharma",
+        dateofReview: "Aug, 2021",
+        userCity: "Gwalior",
+        isCertified: true,
+      },
+      {
+        rating: 4.2,
+        desc: "Such a nice perfume, the fragrance is great and long lasting. This product was suggested by a friend which turned up extremely good. Now I have suggested to my connections. Loved it !!",
+        userName: "Anupam Sharma",
+        dateofReview: "Aug, 2021",
+        userCity: "Gwalior",
+        isCertified: true,
+      },
+    ],
 
-        title: "Autumn Collections",
-        desc: "Fall..The Colors Leave You Breathless, it's Autumn On Our Minds.",
-        isActive: false,
-    },
-    {
-        id: 3,
-        img: "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    isAssured: true,
+    prices: { markpr: 499, sellpr: 356 },
+    offers: [
+      "Bank Offer 5% Unlimited Cashback on Craftsmen Axis Bank Credit Card",
+      "Partner Offer Sign up for Pay Later and get amazing Gift Card worth ₹100*",
+    ],
+    volumeList: [150, 270, 500],
+    // sizeList : ['150', '270', '500'],
+    maxQuantity: 5,
+    highlights: [
+      "Quantity: 20ml, Ideal for Men",
+      "Fragrance Classification: Eau de Parfum",
+      "Fragrance Family: Fresh",
+    ],
+    services: ["No Returns Applicable", "Cash on Delivery available"],
+    desc: "WHEN THE ENERGIES OF THE OCEAN MEET THE FRESHNESS OF CITRUS, A LETHAL VILLAIN IN BORN. GET READY FOR ACTION WITH THE STRONG, MASCULINE FRAGRANCE OF VILLAIN HYDRA. ITS INTENSE AQUATIC NOTES WITH HINTS OF WHITE MUSK KEEP YOU READY FOR THE HUSTLE, WHILE THE ZESTY FRAGRANCE OF FRESH BERGAMOT, LEMON AND PATCHOULI LET YOUR UNPREDICTABILITY AND EXCITEMENT SHINE. PROUD, POWERFUL AND UNIQUE, THE MACHO SCENT OF VILLAIN HYDRA IS MADE FOR THE REBEL IN YOU WHO KNOWS HOW TO GO OUT STRONG AND MAKE IT LAST FOR LONG",
+    expiryDate: "21 Sep, 2023",
+  },
+  {
+    id: 2,
+    img: "https://rukminim1.flixcart.com/image/416/416/kvba7bk0/lunch-box/t/g/e/1600-sparkle-z-2-blue-mastercook-4-original-imag88uyywwqas4c.jpeg?q=70",
 
-        title: "Loungewear Love",
-        desc: "Make yourself fit in gym wear, never go out of style!",
-        isActive: false,
-    },
-    {
-      id: 4,
-      img: "https://images.unsplash.com/photo-1584065736070-43a079541a71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    category: [
+      "Kitchen Cookware & ServeWare",
+      "LunchBoxes",
+      "Master Cook Sparkle Z2",
+    ],
+    name: "MASTER COOK SPARKLE Z 2 BLUE 4 Containers Lunch Box  (1600 ml)",
+    ratingCount: 34,
+    reviews: [
+      {
+        rating: 5,
+        desc: "Fabulous - Very Nice Product !!",
+        userName: "Suraj Paul",
+        dateofReview: "Feb, 2022",
+        userCity: "Jalandhar",
+        isCertified: true,
+      },
+      {
+        rating: 4.8,
+        desc: "Best in the Market, make Office-Lunch Easy and Clean!!",
+        userName: "Ramakant Kharbanda",
+        dateofReview: "Nov, 2021",
+        userCity: "New Delhi",
+        isCertified: true,
+      },
+      {
+        rating: 4.8,
+        desc: "Such a nice product for my kids, it serves the looks as well. Great Design and Durability.",
+        userName: "Kartik Shukla",
+        dateofReview: "Mar, 2021",
+        userCity: "Kolkata",
+        isCertified: true,
+      },
+      {
+        rating: 4.5,
+        desc: "Best in the Market, make Office-Lunch Easy and Clean!!",
+        userName: "Keerti Verareddy",
+        dateofReview: "Nov, 2021",
+        userCity: "Vellore",
+        isCertified: true,
+      },
+      {
+        rating: 5,
+        desc: "Fabulous - Very Nice Product !!",
+        userName: "Suraj Paul",
+        dateofReview: "Feb, 2022",
+        userCity: "Jalandhar",
+        isCertified: true,
+      },
+      {
+        rating: 4.8,
+        desc: "Best in the Market, make Office-Lunch Easy and Clean!!",
+        userName: "Ramakant Kharbanda",
+        dateofReview: "Nov, 2021",
+        userCity: "New Delhi",
+        isCertified: true,
+      },
+      {
+        rating: 4.8,
+        desc: "Such a nice product for my kids, it serves the looks as well. Great Design and Durability.",
+        userName: "Kartik Shukla",
+        dateofReview: "Mar, 2021",
+        userCity: "Kolkata",
+        isCertified: true,
+      },
+      {
+        rating: 4.5,
+        desc: "Best in the Market, make Office-Lunch Easy and Clean!!",
+        userName: "Keerti Verareddy",
+        dateofReview: "Nov, 2021",
+        userCity: "Vellore",
+        isCertified: true,
+      },
+    ],
 
-      title: "Love For Crafts",
-      desc: "Make yourself fall in love with our dear artists!",
-      isActive: false,
+    isAssured: true,
+    prices: { markpr: 925, sellpr: 310 },
+    offers: [
+      "Special PriceGet extra 5% off (price inclusive of discount)",
+      "Combo OfferBuy 2 items save 5%;Buy 3 or more save 10%See all products",
+      "Partner OfferSign up for Flipkart Pay Later and get Flipkart Gift Card worth ₹100*",
+      "Bank OfferFlat ₹50 Instant Cashback on Paytm Wallet. Min Order Value ₹500. Valid once per Paytm account",
+      "Bank Offer5% Unlimited Cashback on Flipkart Axis Bank Credit Card",
+    ],
+    volumeList: [1600, 2400],
+    // sizeList : ['150', '270', '500'],
+    maxQuantity: 8,
+    highlights: [
+      "3 Containers of 300ml::1 Bottle of 700ml::1 Zippered Tiffin Carrier with strap",
+      "Lunch Box Material: Plastic",
+      "Ideal Usage: Office",
+      "Capacity: 1600 ml, Conatiners: 4",
+    ],
+    services: [
+      "ONLY AGAINST MANUFACTURING DEFECTS",
+      "7 Days Replacement Policy",
+      "Cash on Delivery available",
+    ],
+    desc: "SPARKLE Z 2 LUNCH BOX series has a DISHWATER-Safe Feature with leakage resistance, it is also MICROWAVE-safe.. Erogonmicall designed round shaped containers and a firm grip bottle are avilable..",
+    expiryDate: "Sep, 2032",
   },
 ];
 
-export const categories = [{
-        id: 1,
-        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        title: "SHIRT STYLE!",
-        cat: "women"
-    },
-    {
-        id: 2,
-        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        title: "LOUNGEWEAR LOVE",
-        cat: "jeans"
-    },
-    {
-        id: 3,
-        img: "https://assets.myntassets.com/h_480,q_100,w_360/v1/assets/images/7371443/2018/9/29/06658f92-eb53-448a-a5bc-977f5282402e1538213700250-HERENOW-Men-Jackets-5881538213700079-1.jpg",
-        title: "LIGHT JACKETS",
-        cat: "abc"
-    },
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1506152983158-b4a74a01c721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+
+    title: "Summer Sale",
+    desc: "Don't Compromise On Style! Get Flat 30% OFF for New Arrivals!",
+    isActive: true,
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+
+    title: "Autumn Collections",
+    desc: "Fall..The Colors Leave You Breathless, it's Autumn On Our Minds.",
+    isActive: false,
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+
+    title: "Loungewear Love",
+    desc: "Make yourself fit in gym wear, never go out of style!",
+    isActive: false,
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1584065736070-43a079541a71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+
+    title: "Love For Crafts",
+    desc: "Make yourself fall in love with our dear artists!",
+    isActive: false,
+  },
 ];
 
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+    cat: "women",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+    cat: "jeans",
+  },
+  {
+    id: 3,
+    img: "https://assets.myntassets.com/h_480,q_100,w_360/v1/assets/images/7371443/2018/9/29/06658f92-eb53-448a-a5bc-977f5282402e1538213700250-HERENOW-Men-Jackets-5881538213700079-1.jpg",
+    title: "LIGHT JACKETS",
+    cat: "abc",
+  },
+];
 
-  export const popularProducts = [
-    {
-      id:1,
-      img:"https://freepngimg.com/thumb/dress%20shirt/4-black-dress-shirt-png-image.png",
-    },
-    {
-      id:2,
-      img:"https://freepngimg.com/thumb/dress%20shirt/21-dress-shirt-png-image.png",
-    },
-    {
-      id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
-    },
-    {
-      id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
-    },
-    {
-      id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
-    },
-    {
-      id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
-    },
-    {
-      id:7,
-      img:"https://freepngimg.com/thumb/dress/33276-7-striped-dress-image.png",
-    },
-    {
-      id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
-    },
-  ]
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://freepngimg.com/thumb/dress%20shirt/4-black-dress-shirt-png-image.png",
+  },
+  {
+    id: 2,
+    img: "https://freepngimg.com/thumb/dress%20shirt/21-dress-shirt-png-image.png",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://freepngimg.com/thumb/dress/33276-7-striped-dress-image.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
